8388781d6533e3dee9bd4cee05b391134f9902ee
jamesacampbell <james@jamescampbell.us>
2020-11-08 01:21:43 -0500

    I __ a word

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# update the changes
+# update the changes (make sure to run npm run-script build and zip dist.zip to frontend folder)
 git pull
 # clean html folder
-rm -rf /var/www/qrr_codes/html/
-# deploys landing page to /var/www/qrr_codes/html/
-cp -R ../frontend/dist/ /var/www/qrr_codes/html/
+rm -rf /var/www/qrr_codes/html/*
+# copy over zip to staging area
+cp -R ../frontend/dist.zip /var/www/qrr_codes/
+# change directory to staging area
+cd /var/www/qrr_codes/
+# unzip to staging
+unzip /var/www/qrr_codes/dist.zip 
+# deploy code to /var/www/qrr_codes/html/
+cp -R dist/* /var/www/qrr_codes/html/
