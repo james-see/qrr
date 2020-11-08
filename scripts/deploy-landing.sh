7123cffc0a9c98f369c76244e6758e25aec2b1f8
jamesacampbell <james@jamescampbell.us>
2020-11-08 00:03:28 -0500

    Removing unecessary stuff

--- scripts/deploy-landing.sh
+++ scripts/deploy-landing.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# deploys landing page to /var/www/qrr_codes/html/
+# update the changes
+git pull
+# clean html folder
 rm -rf /var/www/qrr_codes/html/
+# deploys landing page to /var/www/qrr_codes/html/
 cp -R ../landing/html/ /var/www/qrr_codes/html/
